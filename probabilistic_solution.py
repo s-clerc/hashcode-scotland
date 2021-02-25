00a1ffc776caa2c9492ffec802152a6c3e662ee3
Sam de Clerc <jaimelelait@icloud.com>
2021-02-25 20:30:40 +0100

    Add prob solution

---
+++ probabilistic_solution.py
@@ -0,0 +1,26 @@
+import fileParser, sys
+from typing import *
+
+data = fileParser.parseFile(sys.argv[1])
+graph = data.graph
+
+street_frequencies = {}
+
+for path in data.cars:
+	for street in path:
+		street_frequencies.setdefault(street, 0)
+		street_frequencies[street] += 1
+
+output = {}
+for intersection in graph.nodes():
+	enterant_names = [data.costreets[street[:2]] 
+					  for street in graph.edges(to_node=intersection)]
+	
+	denominator = 0
+	for name in enterant_names:
+		denominator += street_frequencies.get(name, 0)
+	output[intersection] = [(name, round(denominator/street_frequencies[name])) 
+							for name in enterant_names
+							if street_frequencies.get(name, 0) > 0]
+
+fileParser.outputData(sys.argv[1] + "out", output)
